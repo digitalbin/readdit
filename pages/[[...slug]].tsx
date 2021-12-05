8812d00b6efaa1347123888aaaecaaf202368ffd
Albin Hagström <albin.hagstrom@nentgroup.com>
2021-12-05 18:52:25 +0100

    HAHAHAHA JAJAJAJA

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { useEffect, useState } from 'react';
-import classnames from 'classnames';
+import { useInView } from 'react-intersection-observer';
 import fetch from 'isomorphic-unfetch';
 import Link from 'next/link';
 import type { NextPage, GetServerSideProps } from 'next';
@@ -10,6 +10,7 @@ import ExpandableText from '@components/ExpandableText';
 import VideoPost from '@components/VideoPost';
 import ImagePost from '@components/ImagePost';
 import LinkPost from '@components/LinkPost';
+import classNames from 'classnames';
 
 function getPostType(hint: string) {
     switch (hint) {
@@ -23,6 +24,8 @@ function getPostType(hint: string) {
             return null;
     }
 }
+
+const SubredditIcons = new Map();
 
 const Post = (props: PostData) => {
     const {
@@ -47,86 +50,115 @@ const Post = (props: PostData) => {
     const [subredditIcon, setSubredditIcon] = useState(null);
     const subreddit = `/${subreddit_name_prefixed}`;
 
-    // TODO: dont fetch duplicate icons!!!!!!!!! förväntas av alla som jag känner helst igår!!!!!
+    const { inView, ref } = useInView();
+
     useEffect(() => {
-        fetch(`https://www.reddit.com${subreddit}/about.json`)
-            .then((res) => res.json())
-            .then(({ data: { icon_img, community_icon } }) => {
-              const iconUrl = icon_img || community_icon;
-              const [strippedParams] = iconUrl.split('?')
-              setSubredditIcon(strippedParams)
-            });
-    }, []);
+        if (!inView) return;
+        const subredditIconUrl = SubredditIcons.get(subreddit);
+        if (subredditIconUrl) setSubredditIcon(subredditIconUrl);
+        else {
+            fetch(`https://www.reddit.com${subreddit}/about.json`)
+                .then((res) => res.json())
+                .then(({ data: { icon_img, community_icon } }) => {
+                    const iconUrl = icon_img || community_icon;
+                    const [strippedParams] = iconUrl.split('?');
+                    setSubredditIcon(strippedParams);
+                    SubredditIcons.set(subreddit, strippedParams);
+                });
+        }
+    }, [inView, subreddit]);
 
     const timeAgo = timeSince(created);
 
     const comments = num_comments;
     const upvotes = kFormatter(ups);
 
-    return isCrosspost ? (
-        <div key={id} className="p-6 mb-6 rounded border-2 border-default">
-            <div className="text-default mb-4">
-                <Link href={subreddit}>
-                    <a className="font-bold text-tiny flex items-center">
-                        {subredditIcon ? (
-                          <img src={subredditIcon} alt={`Icon for ${subreddit}`} className="mr-4 w-8 h-8 bg-subtle rounded-full flex-none" />
-                        ) : (
-                          <span className="mr-4 w-8 h-8 bg-subtle rounded-full flex-none" />
-                        )}
-                        <span className="overflow-ellipsis overflow-hidden">
-                            {subreddit_name_prefixed}
-                        </span>
-                        <span className="text-subtle font-regular flex-1 whitespace-nowrap">
-                            &nbsp;•&nbsp;{timeAgo}
-                        </span>
-                    </a>
-                </Link>
-            </div>
-            <Link href={permalink}>
-                <a className="mb-6 block">
-                    <h3>{title}</h3>
-                </a>
-            </Link>
-            <div className="bg-subtle rounded">
-                <Post {...parentProps} />
-            </div>
-            <div className="text-subtle text-tiny mt-6">
-                <span className="mr-4">{comments} comments</span>
-                {upvotes} upvotes
-            </div>
-        </div>
-    ) : (
-        <div key={id} className="p-6 mb-6 rounded border-2 border-default">
-            <div className="text-default mb-4">
-                <Link href={subreddit}>
-                    <a className="font-bold text-tiny flex items-center">
-                        {subredditIcon ? (
-                          <img src={subredditIcon} alt={`Icon for ${subreddit}`} className="mr-4 w-8 h-8 bg-subtle rounded-full flex-none" />
-                        ) : (
-                          <span className="mr-4 w-8 h-8 bg-subtle rounded-full flex-none" />
-                        )}
-                        <span className="overflow-ellipsis overflow-hidden">
-                            {subreddit_name_prefixed}
-                        </span>
-                        <span className="text-subtle font-regular flex-1 whitespace-nowrap">
-                            &nbsp;•&nbsp;{timeAgo}
-                        </span>
-                    </a>
-                </Link>
-            </div>
-            <Link href={permalink}>
-                <a className="mb-6 block">
-                    <h3 className={isCrosspost ? "text-tiny" : "text-tall"}>{title}</h3>
-                </a>
-            </Link>
-            {postType === 'video' && <VideoPost {...props} />}
-            {postType === 'image' && <ImagePost {...props} />}
-            {postType === 'link' && <LinkPost {...props} />}
-            {selftext && <ExpandableText {...props} />}
-            <div className="text-subtle text-tiny mt-6">
-                <span className="mr-4">{comments} comments</span>
-                {upvotes} upvotes
-            </div>
+    return (
+        <div ref={ref}>
+            {isCrosspost ? (
+                <div
+                    key={id}
+                    className="p-6 mb-6 rounded border-2 border-default"
+                >
+                    <div className="text-default mb-4">
+                        <Link href={subreddit}>
+                            <a className="font-bold text-tiny flex items-center">
+                                {subredditIcon ? (
+                                    <img
+                                        src={subredditIcon}
+                                        alt={`Icon for ${subreddit}`}
+                                        className="mr-4 w-8 h-8 bg-subtle rounded-full flex-none"
+                                    />
+                                ) : (
+                                    <span className="mr-4 w-8 h-8 bg-subtle rounded-full flex-none" />
+                                )}
+                                <span className="overflow-ellipsis overflow-hidden">
+                                    {subreddit_name_prefixed}
+                                </span>
+                                <span className="text-subtle font-regular flex-1 whitespace-nowrap">
+                                    &nbsp;•&nbsp;{timeAgo}
+                                </span>
+                            </a>
+                        </Link>
+                    </div>
+                    <Link href={permalink}>
+                        <a className="mb-6 block">
+                            <h3>{title}</h3>
+                        </a>
+                    </Link>
+                    <div className="bg-subtle rounded">
+                        <Post {...parentProps} />
+                    </div>
+                    <div className="text-subtle text-tiny mt-6">
+                        <span className="mr-4">{comments} comments</span>
+                        {upvotes} upvotes
+                    </div>
+                </div>
+            ) : (
+                <article
+                    key={id}
+                    className="p-6 mb-6 rounded border-2 border-default"
+                >
+                    <div className="text-default mb-4">
+                        <Link href={subreddit}>
+                            <a className="font-bold text-tiny flex items-center">
+                                {subredditIcon ? (
+                                    <img
+                                        src={subredditIcon}
+                                        alt={`Icon for ${subreddit}`}
+                                        className="mr-4 w-8 h-8 bg-subtle rounded-full flex-none"
+                                    />
+                                ) : (
+                                    <span className="mr-4 w-8 h-8 bg-subtle rounded-full flex-none" />
+                                )}
+                                <span className="overflow-ellipsis overflow-hidden">
+                                    {subreddit_name_prefixed}
+                                </span>
+                                <span className="text-subtle font-regular flex-1 whitespace-nowrap">
+                                    &nbsp;•&nbsp;{timeAgo}
+                                </span>
+                            </a>
+                        </Link>
+                    </div>
+                    <Link href={permalink}>
+                        <a className="mb-6 block">
+                            <h3
+                                className={classNames({ 'text-tiny': isCrosspost })}
+                            >
+                                {title}
+                            </h3>
+                        </a>
+                    </Link>
+                    {postType === 'video' && <VideoPost {...props} />}
+                    {postType === 'image' && <ImagePost {...props} />}
+                    {postType === 'link' && <LinkPost {...props} />}
+                    {selftext && <ExpandableText {...props} />}
+                    <div className="text-subtle text-tiny mt-6">
+                        <span className="mr-4">{comments} comments</span>
+                        {upvotes} upvotes
+                    </div>
+                </article>
+            )}
         </div>
     );
 };
@@ -143,41 +175,10 @@ const Home: NextPage<IProps> = (props) => {
         <main className="p-4">
             {props.kind === 'Listing' &&
                 props.data.children.map((post: IPost) => (
-                    <Post key={post.data.id} {...post.data} />
+                    <section key={post.data.id}>
+                        <Post {...post.data} />
+                    </section>
                 ))}
-            {/* <h2>{title}</h2>
-          <img src={url} /> */}
-            {/* <ul>
-          {children.map(child => {
-            const {
-              data,
-              data: {
-                title,
-                preview,
-                thumbnail,
-                id,
-                permalink
-              }
-            } = child;
-            console.log(data);
-            
-            return (
-              <li key={id} style={{
-                marginBottom: 16,
-              }}>
-                <Link href={permalink}>
-                  <a style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                  }}>
-                  <span>{title}</span>
-                  <img src={thumbnail} />
-                  </a>
-                </Link>
-              </li>
-            )
-          })}
-        </ul> */}
         </main>
     );
 };
@@ -187,6 +188,8 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const res = await fetch(`https://www.reddit.com${resolvedUrl}.json`).then(
         (res) => res.json(),
     );
+    // console.log(Array.isArray(res));
+
     return {
         props: res,
     };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className="container"><Component {...pageProps} /></div>
+  return <div className="mx-auto max-w-5xl"><Component {...pageProps} /></div>
 }
 
 export default MyApp
